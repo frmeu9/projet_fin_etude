da3711498b4634dd2c2460734439f5385b01f3ca
Françoise Meunier <francoise.meunier.1@ulaval.ca>
2019-09-27 13:48:57 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,31 @@
-" FICHIER POUR DES TESTS "
+' FICHIER POUR TESTS '
 
 import tkinter as tk
 from tkinter import filedialog
+import cv2
+import goprocam
 
-import lib.goprocam
 
-# Pour aller chercher une fichier, l'ouvrir et print le contenu
-root = tk.Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename()
 
-f = open(file_path)
-contents =f.read()
-print(contents)
+# Pour aller chercher un fichier.txt, l'ouvrir et print le contenu
+# def getFile():
+#     root = tk.Tk()
+#     root.withdraw()
+#     file_path = filedialog.askopenfilename()
+#     return file_path
+#
+# # Pour afficher une image
+# def show_img():
+#     file_path = getFile()
+#     img = cv2.imread(file_path)
+#     cv2.imshow('image',img)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
 
+#######################
+from goprocam import GoProCamera
+from goprocam import constants
+import time
+gpCam = GoProCamera.GoPro(constants.auth)
+gpCam.overview()
+gpCam.listMedia(True)
