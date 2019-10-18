54950f1f2e5c701f58b47a8ee7e1f84c3437de93
Françoise Meunier <francoise.meunier.1@ulaval.ca>
2019-10-18 13:52:34 -0400

    try cv2.addWeighted

--- test.py
+++ test.py
@@ -125,7 +125,8 @@
 #         self.master = master
 #         master.title("Download")
 #
-#         self.buttonGetFileFromComputer = tk.Button(master, text="Get file from computer", command=self.getFileFromComputer)
+#         self.buttonGetFileFromComputer = tk.Button(master, text="Get file from computer",
+#         command=self.getFileFromComputer)
 #         self.buttonGetFileFromComputer.pack(fill=tk.X)
 #
 #         self.buttonGetFileFromCamera = tk.Button(master, text="Get file from Camera", command=self.getFileFromCamera)
@@ -251,8 +252,36 @@
 #
 # import numpy as np
 #
-# image = mpimg.imread("C:/Users/meuni/Desktop/ÉCOLE/UNI/Session A4/Projet de fin d'étude/projet_fin_etude/src/image/softdb.png")
+# image = mpimg.imread("C:/Users/meuni/Desktop/ÉCOLE/UNI/Session A4/
+# Projet de fin d'étude/projet_fin_etude/src/image/softdb.png")
 # print(image)
 #
 # plt.imshow(image, cmap="viridis")
 # plt.show()
+#########################################################
+# Python program for blending of
+# images using OpenCV
+
+# import OpenCV file
+# import cv2
+#
+# _, src, dst = perspective_transform(image, corners)
+# Minv = cv2.getPerspectiveTransform(dst, src)
+#
+# # Create an image to draw the lines on
+# warp_zero = np.zeros_like(image[:, :, 0]).astype(np.uint8)
+# color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+#
+# # Recast the x and y points into usable format for cv2.fillPoly()
+# pts = np.vstack((fitx, ploty)).astype(np.int32).T
+#
+# # Draw the lane onto the warped blank image
+# # plt.plot(left_fitx, ploty, color='yellow')
+# cv2.polylines(color_warp, [pts], False, (0, 255, 0), 10)
+# # cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
+#
+# # Warp the blank back to original image space using inverse perspective matrix (Minv)
+# newwarp = cv2.warpPerspective(color_warp, Minv, (image.shape[1], image.shape[0]))
+#
+# # Combine the result with the original image
+# result = cv2.addWeighted(image, 1, newwarp, 0.3, 0)
